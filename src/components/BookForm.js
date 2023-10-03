94daa062de8c3e2cb8512bc29593dc61760b7ac8
ThomasBrets <thomas15501331@gmail.com>
2023-10-02 22:10:35 -0300

    Start Check in

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
+// Components
+import CheckIn from "../components/CheckIn"
+import CheckOut from "../components/KidsDropdown"
+import AdultsDropdown from "../components/AdultsDropdown"
+import KidsDropdown from "../components/KidsDropdown"
+
+
 const BookForm = () => {
-  return <div>BookForm</div>;
+  return <form className='h-[300px] bg-slate-300 w-full lg:h-[70px]'>
+    <div className='flex flex-col w-full h-full lg:flex-row'>
+      <div className="flex-1 border-r">
+        <CheckIn />
+      </div>
+      <div className="flex-1 border-r">
+        <CheckOut />
+      </div>
+      <div className="flex-1 border-r">
+        <AdultsDropdown />
+      </div>
+      <div className="flex-1 border-r">
+        <KidsDropdown />
+      </div>
+    </div>
+    </form>;
 };
 
 export default BookForm;
