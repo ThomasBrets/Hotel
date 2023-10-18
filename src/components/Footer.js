c49b3750a3fe4c5bd5f5910b7eda53a89b05ea61
ThomasBrets <thomas15501331@gmail.com>
2023-10-18 18:47:01 -0300

    finished home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
-// import React from 'react';
+import React from 'react';
+// Logo
+import logoWhite from "../assets/img/logo-white.svg"
+
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className='bg-primary py-12'>
+    <div className="container mx-auto text-white flex justify-between">
+      <a href="/">
+      <img src={logoWhite} alt=""/>
+      </a>
+      Copyright &copy; 2023. All rights reserved.
+    </div>
+    </footer>;
 };
 
 export default Footer;
